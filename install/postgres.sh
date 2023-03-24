00f5566224e7474cb569d5d7c224582b64f5d884
bsgabriel <gabrielbs22@icloud.com>
2023-03-23 21:33:08 -0300

    Correção no arquivo de instalação do postgresql e pgadmin

--- install/postgres.sh
+++ install/postgres.sh
@@ -1,6 +1,9 @@
+
 # pgAdmin apt repository
 sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add;
 sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list';
+
+sudo apt update;
 
 echo "";
 echo "------- Installing postgresql -------";
